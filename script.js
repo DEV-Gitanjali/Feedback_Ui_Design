344171331258a156a6fbe311e497c162c998f2ad
Dev-Gitanjali <jgitanjali592@gmail.com>
2024-02-15 22:47:17 +0530

    this method target rating classlist

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const   ratings=document.querySelectorAll('.rating')
 const   sendBtn=document.querySelector('#send')
 const   panel=document.querySelectorAll('#panel')
+
+
+panel.addEventListener('click',(e)=>{
+
+    if(e.target.classList.contains('rating')){
+        e.target.parentNode.classList.add('active')
+
+        removeActive()
+
+        e.target.parentNode.classList.add('active')
+    }
+})
+
+
+function removeActive(){
+    
+}
